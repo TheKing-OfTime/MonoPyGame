5d284abf07aec32d7b22477f417ff9f85186fa8d
TheKing-OfTime <artem.matvieno0@gmail.com>
2023-03-26 15:23:07 +0300

    New

--- classes/Displayable.py
+++ classes/Displayable.py
@@ -7,20 +7,30 @@ from classes.BaseClass import BaseClass
 class Displayable(BaseClass):
 	asset = None
 	pos = None
+	_show: bool = False
 
-	def __init__(self, scene):
+	def __init__(self, scene, show=False):
 		super().__init__(scene)
-		self.pos = Position(scene, length=10, height=10)
+		self.pos = Position(scene, length=100, height=100)
+		self._show = show
 
 	def load_asset(self, asset_path):
 		self.asset = pygame.image.load(asset_path)
 
 	# noinspection PyTypeChecker
 	def draw(self):
+		if not self._show:
+			return
 		if self.asset is None:
 			pygame.draw.rect(self.scene, self.pos.color, self.pos.get_rect())
 		else:
 			self.scene.blit(self.asset, (self.pos.x, self.pos.y))
+
+	def show(self):
+		self._show = True
+
+	def hide(self):
+		self._show = False
 
 
 class Position(BaseClass):
@@ -29,7 +39,7 @@ class Position(BaseClass):
 	length = 0
 	height = 0
 
-	def __init__(self, scene, x=100, y=100, length=1, height=1):
+	def __init__(self, scene, x=100, y=100, length=10, height=10):
 		super().__init__(scene)
 		self.x = x
 		self.y = y
@@ -38,8 +48,16 @@ class Position(BaseClass):
 		self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 	def get_rect(self):
-		return ((self.x - (self.length / 2)), (self.y - (self.height / 2)), (self.x + (self.length / 2)),
-				(self.y + (self.height / 2)))
+		print(self.x, self.y, self.length, self.height)
+		return self.x, self.y, self.length, self.height
+
+	def move(self, x, y):
+		self.x += x
+		self.y += y
+
+	def move_to(self, x, y):
+		self.x = x
+		self.y = y
 
 
 class Animated(Displayable):
@@ -58,4 +76,3 @@ class Animated(Displayable):
 			pygame.draw.rect(self.scene, self.pos.color, self.pos.get_rect())
 		else:
 			self.scene.blit(self.assets[asset_number], (self.pos.x, self.pos.y))
-
