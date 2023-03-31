775a19248b8f2deca6c05b2562e4da6e756fb865
Матвиенко Артём <artem.matvienko0@gmail.com>
2023-03-31 16:29:36 +0300

    Add Game class

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import pygame
-from classes.Displayable import Displayable, Animated
-from classes.GameBackgroud import GameBackground
+from classes.Game import Game
 
 pygame.init()
 scr = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Monopoly')
-done = False
-test_displayable = Animated(scr)
-bg = GameBackground(scr)
-test_displayable.load_asset('assets/tiles/green_1')
-while not done:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-	bg.draw()
-	test_displayable.draw(1)
-	test_displayable.pos.move_to(100, 100)
-	if not test_displayable._show:
-		test_displayable.show()
-	pygame.display.flip()
-	pygame.time.wait(10)
-	scr.fill(color=[0, 0, 0])
+
+game = Game(scr)
+
+game.run_loop()
