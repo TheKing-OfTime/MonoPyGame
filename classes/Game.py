f44207ce35434e5022d28418d885351ba23f5de3
TheKing-OfTime <artem.matvienko0@gmail.com>
2023-04-11 10:27:58 +0300

    Add game states

--- classes/Game.py
+++ classes/Game.py
@@ -5,13 +5,21 @@ from classes.GameBackgroud import GameBackground
 
 class Game(BaseClass):
 
+	GAME_STATES = [
+		"LOADING"
+		"PAUSED"
+		"DEFAULT"
+		"MAIN_MENU"
+		"CONNECTING"
+	]
 	def __init__(self, scene):
 		super().__init__(scene)
 		self.done = False
-		self.game_stage = "LOADING"
+		self.game_state = "LOADING"
 		self.test_displayable = Animated(scene)
 		self.bg = GameBackground(scene)
 		self.test_displayable.load_asset('assets/tiles/green_1')
+		self.game_state = "DEFAULT"
 
 
 	def run_loop(self):
@@ -20,7 +28,7 @@ class Game(BaseClass):
 				if event.type == pygame.QUIT:
 					self.done = True
 			self.bg.draw()
-			self.test_displayable.draw_next()
+			self.test_displayable.draw()
 			self.test_displayable.pos.move_to(100, 100)
 			if not self.test_displayable._show:
 				self.test_displayable.show()
