5d4289b08f5f73c149f512fcb4aa985ea062bde9
Valkvo <dnl4>
2023-04-16 16:30:47 +0300

    Any f*ckin class

---
+++ classes/newClass.py
@@ -0,0 +1,10 @@
+from classes.BaseClass import BaseClass
+
+class NewClass(BaseClass):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.peremenaya = 0
+
+
+    def func(self):
+        pass
