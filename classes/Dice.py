9aebddc7cf6c77bcfc8bc8963cdb917f612c575a
Нестор Евсеев <nestorkot0@ya.ru>
2023-04-30 12:13:36 +0000

    идиотизм

--- classes/Dice.py
+++ classes/Dice.py
@@ -1,11 +1,28 @@
 from classes.Displayable import Displayable
 from random import randint
 
-
-class Dice(Displayable):
+# TODO: переписать
+class Dice(Animated):
     def __init__(self, scene):
         super().__init__(scene)
         self.value = 0
 
     def roll(self):
         self.value = randint(1, 6)
+        
+
+def GlassDice(Displayable):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.first_dice = Dice(scene, True)
+        self.second_dice = Dice(scene, True)
+    def roll_dices(self):
+        self.first_dice.roll()
+        self.second_dice.roll()
+        while (self.first_dice.value == self.second_dice.value):
+            self.first_dice.roll()
+            self.second_dice.roll()
+        result = self.first_dice.value+self.second_dice.value
+        self.first_dice.value, self.second_dice.value = 0
+        return result
+        
